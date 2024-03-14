9ea45a996037aacf15be6c2f0f6c09a76d6c0c93
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-14 17:22:31 -0300

    Passo 5

---
+++ exercicioCincoeSeis.js
@@ -0,0 +1,13 @@
+const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14];
+
+let pares = [];
+
+for (let i = 0; i < numeros.length; i++) {
+    
+    if(numeros[i]%2 == 0){
+        pares.push(numeros[i]);
+    }
+    
+}
+
+console.log(pares);
