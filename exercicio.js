60a68bf14dea0d63699a8359de92800d785b6ca5
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-14 16:50:17 -0300

    Passo 2

--- exercicio.js
+++ exercicio.js
@@ -3,3 +3,11 @@ const array = [1,2,3,4,5,6,7];
 for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
 }
+
+function imprimeArray(array){
+    for (let i = 0; i < array.length; i++) {
+        console.log(`Index: ${i} valores: ${array[i]}`);
+    }
+}
+
+imprimeArray(array);
